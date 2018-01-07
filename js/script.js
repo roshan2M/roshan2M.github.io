16402042a8afaae8358f0dd6903a27152598961b
Roshan Munjal <roshan.munjal@hotmail.com>
2018-01-07 16:41:26 -0500

    New Website First Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+ * Please see the included README.md file for license terms and conditions.
+ */
+
+// This file is a suggested starting place for your code.
+// It is completely optional and not required.
+// Note the reference that includes it in the index.html file.
+
+/*
+ * The jQuery code to begin a script. Will be used if needed.
+ */
+$(document).ready(function() {
+    
+});
